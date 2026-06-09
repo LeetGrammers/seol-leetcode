020b3c065bce74d666f71f996465712f328a1dce
Seol <61292867+lxeso@users.noreply.github.com>
2026-06-09 11:24:09 +0900

    Time: 0 ms (100%), Space: 41.7 MB (95.35%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,24 +1,18 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        	int[][] grid = new int[m][n];
-            return dp(m-1, n-1, grid);
+        			int[][] dp = new int[m][n];
+		for (int k = 0; k < m; k++) {
+			dp[k][0] = 1;
+		}
+		for (int x = 0; x < n; x++) {
+			dp[0][x] = 1;
+		}
+
+		for (int i = 1; i < m; i++) {
+			for (int j = 1; j < n; j++) {
+				dp[i][j] = dp[i][j-1] + dp[i-1][j];
+			}
+		}
+		return dp[m-1][n-1];
     }
-
-    	public int dp(int i, int j, int[][] memo){
-		if (i < 0 || j < 0) { // 구간 유효범위 벗어났을때, 재귀호출에 i-1과 같은 인자를 넘겨주기 때문에 구간을 초과하진 않고 음수되는 부분만 신경써줌
-			return 0;
-		}
-
-		if (i == 0 && j == 0) { // 초기값 세팅 (0,0)에 서있는 방법 1가지에서 출발
-			return 1;
-		}
-
-		if (memo[i][j] != 0) { // 메모이제이션 : 저장된 값이 있을경우 해당값 리턴
-			return memo[i][j];
-		}
-
-		memo[i][j] = dp(i, j-1, memo)  + dp(i-1, j, memo); // 내 현재위치 기준 왼쪽에서 오는거랑 위에서 내려오는거 두개
-
-		return memo[i][j];
-	}
 }

46baa5b309281d544ad10bce45ffe6d36fbd2b73
Seol <61292867+lxeso@users.noreply.github.com>
2026-08-12 14:48:50 +0900

    Time: 6 ms (82.9%), Space: 46.1 MB (29.63%) - LeetHub

---
+++ 1035-uncrossed-lines/1035-uncrossed-lines.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int maxUncrossedLines(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+        int[][] dp = new int[m+1][n+1]; // nums1에서 i개, nums2에서 j개 썼을때 만들 수 있는 최대 uncrossed line 수를 저장
+
+        dp[0][0] = 0;
+
+        for (int k = 0; k <= m; k++) {
+            dp[k][0] = 0;
+        }
+        for (int l = 0; l <= n; l++) {
+            dp[0][l] = 0;
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (nums1[i-1] == nums2[j-1]) {
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+}
